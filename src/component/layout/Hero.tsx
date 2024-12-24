e0b57003c09bfb2f4b4fc03affb265fbed7c0a14
Dzalfar <hafidzalghi@gmail.com>
2024-12-25 04:25:48 +0700

    feat: init

--- src/component/layout/Hero.tsx
+++ src/component/layout/Hero.tsx
@@ -11,7 +11,7 @@ function Hero() {
       <div className="flex desktop:justify-between desktop:flex-row desktop:items-center mobile:flex-col items-center w-full flex-1 gap-8">
         <div className="flex flex-col z-20 pointer-events-none text-white desktop:w-3/5 mobile:w-full mobile:text-center">
           <p className="desktop:text-[5vw] mobile:text-[8vw] font-bold text-white w-full">
-            Hallo , &apos; My Name Euis siti meida'
+            Hallo , &apos; My Name Euis siti meida
           </p>
           <p className="desktop:text-[3vw] mobile:text-[5vw]">
             a developer specializing in web applications with
@@ -21,7 +21,6 @@ function Hero() {
             <SocialMedia size={200} />
           </div>
         </div>
-
         <img
           className="w-1/5 mobile:w-3/5 mobile:max-w-sm object-contain self-center animate-scalepulse"
           src="/euis.png"
